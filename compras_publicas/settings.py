759392b6d9b8336f9ae21f80f51cc35d5d8735ac
jcardenasc93 <jcardenasc93@gmail.com>
2020-06-24 23:28:42 -0500

    Agrega landingPage app

--- compras_publicas/settings.py
+++ compras_publicas/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'compras_publicas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
