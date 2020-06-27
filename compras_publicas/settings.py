28244da9567c33b7b901baabf486abb189ac5ac7
jcardenasc93 <jcardenasc93@gmail.com>
2020-06-26 22:05:41 -0500

    Agrega configuracion de base de datos

--- compras_publicas/settings.py
+++ compras_publicas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'compras_publicas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DBNAME', None),
+        'USER': os.environ.get('DBUSER', None),
+        'PASSWORD': os.environ.get('DBPASS', None),
+        'HOST': os.environ.get('DBHOST', None),
+        'PORT': os.environ.get('DBPORT', None),
     }
 }
 
