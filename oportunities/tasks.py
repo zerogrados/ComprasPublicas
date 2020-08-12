909bf9a46d39e1da41fa5082ceef102b31fd0956
jcardenasc93 <jcardenasc93@gmail.com>
2020-08-12 00:09:34 -0500

    Actualiza tipo de dato para el campo valor_proceso de Oportunidad, se crea funcionalidad para hacer un match de oportunidades basados en los valores de presupuesto y ciudad de un Perfil. Se crea funcion de utulidad para actualizar las oportunidades

--- oportunities/tasks.py
+++ oportunities/tasks.py
@@ -1,22 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .models import Oportunidad
-import requests
-import json
+from .utilities.update_oportunities import updateOportunity
 import logging
 import datetime
 from oportunities.utilities.insert_oportunities import get_request
 
-logging.basicConfig(filename='../update_oportunities.log',level=logging.ERROR)
+logging.basicConfig(filename='../update_oportunities.log', level=logging.ERROR)
 
-def updateOportunity(oportunity):
-    """This method update oportunity in database.
-    """
-    # Check source of oportunity, 1: SECOPI, 2: SECOPII
-    if oportunity[1] == 1:
-        response = requests.get('https://www.datos.gov.co/resource/c82b-7jfi.json?numero_de_constancia={}'.format(oportunity[0]))
-        resp = response.json()[0]
-        process = Oportunidad.objects.filter(num_proceso=oportunity[0]).update(estado_proceso=resp['estado_del_proceso'])
 
 @shared_task
 def insertOportunitiesToUpdate():
@@ -28,12 +19,13 @@ def insertOportunitiesToUpdate():
             updateOportunity(oportunity)
         except:
             logging.error('Oportunity cannot be update: ' + oportunity[0])
-            
+
+
 @shared_task
 def insertOportunities():
     for day in range(3):
         # Check the API for each day since one month ago
         date = (datetime.datetime.today() -
                 datetime.timedelta(days=day)).strftime('%m/%d/%Y')
-        
+
         get_request(date)
