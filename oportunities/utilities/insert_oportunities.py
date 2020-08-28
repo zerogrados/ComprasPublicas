4c374116e2d20444e31118640299d99ca4c2092f
Cristian Martinez <cristianmartinez@MacBook-Pro-de-Cristian.local>
2020-08-28 01:24:12 -0500

    Refactoriza funciones para busqusqueda de municipio ejecucion y municipio entidad

--- oportunities/utilities/insert_oportunities.py
+++ oportunities/utilities/insert_oportunities.py
@@ -18,7 +18,7 @@ def get_city_by_alias(city):
     """Search for a city by ciudad_alias.
        This method is called only when search by ciudad_lower return None.
     """
-    result = Ciudad.objects.filter(ciudad_alias=city)
+    result = Ciudad.objects.filter(ciudad_alias=city).values_list('codigo_ciudad', flat=True)
     if len(result) == 1:
         return result[0]
     else:
@@ -32,9 +32,9 @@ def get_city(city, department=None):
     if department:
         department = department.lower().strip()
         result = Ciudad.objects.filter(
-            ciudad_lower=city, departamento_lower=department)
+            ciudad_lower=city, departamento_lower=department).values_list('codigo_ciudad', flat=True)
     else:
-        result = Ciudad.objects.filter(ciudad_lower=city)
+        result = Ciudad.objects.filter(ciudad_lower=city).values_list('codigo_ciudad', flat=True)
 
     if len(result) == 1:
         return result[0]
@@ -49,21 +49,6 @@ def get_city(city, department=None):
             # Register log with city not found
             logging.critical('City not found in database: ' + city)
             return 90000
-
-
-def validate_city(city):
-    """This method validate if municipio_entidad exists in a database.
-    """
-    if city != None:
-        return True, city
-    else:
-        # Validate if city exists in ciudad_alias field
-        city_alias = get_city_by_alias(city)
-        if city_alias != None:
-            return False, city_alias
-        else:
-            # If not exist, return foreing key of No existe object
-            return True, 90000
 
 
 def search_city_SECOPI(oportunity_data):
@@ -89,7 +74,7 @@ def search_city_SECOPI(oportunity_data):
     if ((municipio_ejecucion == 'no definido') and (municipio_entrega == 'no definido') and (municipio_obtencion == 'no definido')):
         # Validate if municipio_ejecucion and municipio_entrega and municipio_obtencion are not defined, return ubication of entity
         city = get_city(municipio_entidad, department=depto_entidad)
-        return validate_city(city)
+        return True, city
     else:
         if municipio_ejecucion != 'no definido':
             # if municipio_ejecucion exist in a database, check if are more than one
@@ -99,13 +84,13 @@ def search_city_SECOPI(oportunity_data):
                 municipio_ejecucion = municipios[0]
             department, city = municipio_ejecucion.split('-')
             city = get_city(city, department=department)
-            if city != None:
+            if city != 90000:
                 # If city exist, return city
                 return False, city
 
         if municipio_entrega != 'no definido':
             city = get_city(municipio_entrega)
-            if city != None:
+            if city != 90000:
                 # Check if exist more than one city with the same name
                 if city == 1:
                     city = get_city(municipio_entidad,
@@ -114,14 +99,14 @@ def search_city_SECOPI(oportunity_data):
                     if municipio_entrega == municipio_entidad:
                         return False, city
                     else:
-                        return validate_city(city)
+                        return True, city
                 elif city != 90000:
                     # If city is diferent to undefined, return city
                     return False, city
 
         if municipio_obtencion != 'no definido':
             city = get_city(municipio_obtencion)
-            if city != None:
+            if city != 90000:
                 # Check if exist more than one city with the same name
                 if city == 1:
                     city = get_city(municipio_entidad,
@@ -130,13 +115,15 @@ def search_city_SECOPI(oportunity_data):
                     if municipio_obtencion == municipio_entidad:
                         return False, city
                     else:
-                        return validate_city(city)
+                        return True, city
                 else:
                     return False, city
+            else:
+                return True, city
         else:
             # If not exist, return municipio_entidad key
             city = get_city(municipio_entidad, department=depto_entidad)
-            return validate_city(city)
+            return True, city
 
 
 def validate_codunspsc(data):
@@ -161,19 +148,18 @@ def insert_oportunity_secop_i(data):
         # The process doesn't exist in the DB
         city_entity = get_city(
             data['municipio_entidad'], department=data['departamento_entidad'])
-        undefined, city_entity = validate_city(city_entity)
         undefined, city_process = search_city_SECOPI(data)
         pub_date = datetime.strptime(data['fecha_de_cargue_en_el_secop'], '%m/%d/%Y')
         data = validate_codunspsc(data)
         oportunity = Oportunidad(num_proceso=data['numero_de_constancia'], cod_unspsc_id=data['id_objeto_a_contratar'],
                                  cod_unspsc_familia_id=data['id_familia'] + '0000', cod_unspsc_clase_id=data['id_clase'] + '00',
                                  estado_proceso=data['estado_del_proceso'], fuente=1, entidad=data['nombre_de_la_entidad'],
-                                 municipio_entidad_id=city_entity.codigo_ciudad, nit_entidad=data[
+                                 municipio_entidad_id=city_entity, nit_entidad=data[
                                      'nit_de_la_entidad'], objeto_proceso=data['objeto_a_contratar'],
                                  detalle_objeto_proceso=data['detalle_del_objeto_a_contratar'], valor_proceso=data['cuantia_proceso'],
                                  id_tipo_proceso=int(data['id_tipo_de_proceso']), tipo_proceso=data['tipo_de_proceso'], 
                                  fecha_publicacion=pub_date, plazo_ejecucion_cant=-1, plazo_ejecucion_und='', 
-                                 municipio_ejecucion_id=city_process.codigo_ciudad, url_proceso=data['ruta_proceso_en_secop_i']['url'], 
+                                 municipio_ejecucion_id=city_process, url_proceso=data['ruta_proceso_en_secop_i']['url'], 
                                  undefined_flag=undefined)
 
         try:
@@ -191,7 +177,7 @@ def insert_oportunity_secop_i(data):
 def get_request(date):
     # This method consult the API passing the date as aparameter to the query
     response = requests.get('https://www.datos.gov.co/resource/c82b-7jfi.json?fecha_de_cargue_en_el_secop=' +
-                            date + '&estado_del_proceso=Convocado&$limit=3')
+                            date + '&estado_del_proceso=Convocado&$limit=1000')
 
     resp = response.json()
 
